37cc66cbf107d4483cf963755e195a21600e9f6d
Benjamin Fabbio <benjaminfabbio@gmail.com>
2026-09-06 21:36:41 -0300

    mapeos categoria y usuarios

---
+++ backend/src/SubastaYa.Aplicacion/Mapeos/UsuarioMapeos.cs
@@ -0,0 +1,19 @@
+﻿using SubastaYa.Aplicacion.DTOs;
+using SubastaYa.Dominio.Entidades;
+
+namespace SubastaYa.Aplicacion.Mapeos;
+
+public static class UsuarioMapeos
+{
+    public static UsuarioDto MapeoDto(this Usuario entidad)
+    {
+        return new UsuarioDto
+        {
+            Id = entidad.Id,
+            Alias = entidad.Alias,
+            Email = entidad.Email,
+            Rol = entidad.Rol,
+            FechaRegistro = entidad.FechaRegistro
+        };
+    }
+}
