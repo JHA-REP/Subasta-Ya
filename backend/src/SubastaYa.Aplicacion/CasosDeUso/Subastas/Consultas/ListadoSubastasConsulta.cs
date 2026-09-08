751ba35118cf1a3bdedf3ca6b2bdd54a9bc075c7
Benjamin Fabbio <benjaminfabbio@gmail.com>
2026-09-08 16:45:36 -0300

    filtros, datos de paginacion y resultadopaginadodto

--- backend/src/SubastaYa.Aplicacion/CasosDeUso/Subastas/Consultas/ListadoSubastasConsulta.cs
+++ backend/src/SubastaYa.Aplicacion/CasosDeUso/Subastas/Consultas/ListadoSubastasConsulta.cs
@@ -1,6 +1,15 @@
-﻿namespace SubastaYa.Aplicacion.CasosDeUso.Subastas.Consultas;
+﻿using SubastaYa.Dominio.Enumeraciones;
+
+namespace SubastaYa.Aplicacion.CasosDeUso.Subastas.Consultas;
 
 public class ListadoSubastasConsulta
 {
+    public EstadoSubasta? Estado { get; set; }
+    public int? CategoriaId { get; set; }
+    public decimal? PrecioMin { get; set; }
+    public decimal? PrecioMax { get; set; }
+    public int Pagina { get; set; } = 1;
+    public int TamanoPagina { get; set; } = 10;
 }
-//por el momento lo dejo vacio, ya que no se necesita ningun parametro para listar todas las subastas
+
+//los que tienen "?" son opcionales, los que no tienen son obligatorios.
---
+++ backend/src/SubastaYa.Aplicacion/DTOs/ResultadoPaginadoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SubastaYa.Aplicacion.DTOs;
+
+public class ResultadoPaginadoDto<T>
+{
+    public IEnumerable<T> Items { get; set; } = [];
+    public int TotalItems { get; set; }
+    public int Pagina { get; set; }
+    public int TamanoPagina { get; set; }
+    public int TotalPaginas { get; set; }
+}
