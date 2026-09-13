d4fd0301c108209710b176a88120b7fa6b50b815
Benjamin Fabbio <benjaminfabbio@gmail.com>
2026-09-13 20:23:42 -0300

    carga manual de categoria y vendedor en NuevaSubastaManejador

--- backend/src/SubastaYa.Aplicacion/CasosDeUso/Subastas/Manejadores/NuevaSubastaManejador.cs
+++ backend/src/SubastaYa.Aplicacion/CasosDeUso/Subastas/Manejadores/NuevaSubastaManejador.cs
@@ -9,13 +9,19 @@ namespace SubastaYa.Aplicacion.CasosDeUso.Subastas.Manejadores;
 public class NuevaSubastaManejador
 {
     private readonly IRepositorio<Subasta> _repositorioSubastas;
+    private readonly IRepositorio<Categoria> _repositorioCategorias;
+    private readonly IRepositorio<Usuario> _repositorioUsuarios;
     private readonly IUnidadDeTrabajo _unidadDeTrabajo;
 
     public NuevaSubastaManejador(
         IRepositorio<Subasta> repositorioSubastas,
+        IRepositorio<Categoria> repositorioCategorias,
+        IRepositorio<Usuario> repositorioUsuarios,
         IUnidadDeTrabajo unidadDeTrabajo)
     {
         _repositorioSubastas = repositorioSubastas;
+        _repositorioCategorias = repositorioCategorias;
+        _repositorioUsuarios = repositorioUsuarios;
         _unidadDeTrabajo = unidadDeTrabajo;
     }
 
@@ -38,6 +44,10 @@ public class NuevaSubastaManejador
 
         await _repositorioSubastas.AltaAsync(subasta);
         await _unidadDeTrabajo.ConfirmacionAsync();
+
+        // carga manual de relaciones para que el DTO devuelva categoria y vendedor
+        subasta.Categoria = await _repositorioCategorias.PorIdAsync(subasta.CategoriaId);
+        subasta.Vendedor = await _repositorioUsuarios.PorIdAsync(subasta.VendedorId);
 
         return subasta.MapeoDto();
     }
