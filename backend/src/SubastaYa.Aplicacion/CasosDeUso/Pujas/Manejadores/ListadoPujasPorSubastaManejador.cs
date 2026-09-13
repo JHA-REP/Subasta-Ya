063da145779f3f33943be15c102c09f8ca321169
Benjamin Fabbio <benjaminfabbio@gmail.com>
2026-09-13 20:32:17 -0300

    cargar alias de postor y lider anonimizado en ListadoPujasPorSubastaManejador"

--- backend/src/SubastaYa.Aplicacion/CasosDeUso/Pujas/Manejadores/ListadoPujasPorSubastaManejador.cs
+++ backend/src/SubastaYa.Aplicacion/CasosDeUso/Pujas/Manejadores/ListadoPujasPorSubastaManejador.cs
@@ -9,15 +9,23 @@ namespace SubastaYa.Aplicacion.CasosDeUso.Pujas.Manejadores;
 public class ListadoPujasPorSubastaManejador
 {
     private readonly IRepositorio<Puja> _repositorioPujas;
+    private readonly IRepositorio<Usuario> _repositorioUsuarios;
 
-    public ListadoPujasPorSubastaManejador(IRepositorio<Puja> repositorioPujas)
+    public ListadoPujasPorSubastaManejador(IRepositorio<Puja> repositorioPujas, IRepositorio<Usuario> repositorioUsuarios)
     {
         _repositorioPujas = repositorioPujas;
+        _repositorioUsuarios = repositorioUsuarios;
     }
 
     public async Task<IEnumerable<PujaDto>> EjecucionAsync(ListadoPujasPorSubastaConsulta consulta)
     {
         var pujas = await _repositorioPujas.FiltradasAsync(p => p.SubastaId == consulta.SubastaId);
-        return pujas.Select(p => p.MapeoDto());
+        var usuarios = await _repositorioUsuarios.TodosAsync();
+        
+                return pujas.Select(p =>
+                {
+            var alias = usuarios.FirstOrDefault(u => u.Id == p.PostorId)?.Alias;
+                        return p.MapeoDto(alias);
+                    });
     }
 }
