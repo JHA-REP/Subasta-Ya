f5f8b3406687150e2369788774e4cdf7bad01a9a
Benjamin Fabbio <benjaminfabbio@gmail.com>
2026-09-16 17:06:23 -0300

    ruta POST de pujas movida a /subastas/{subastaId}/pujas

--- backend/src/SubastaYa.API/Controladores/PujasControlador.cs
+++ backend/src/SubastaYa.API/Controladores/PujasControlador.cs
@@ -6,11 +6,11 @@ namespace SubastaYa.Api.Controladores;
 
 //  endpoint de pujas con manejadores cqrs
 [ApiController]
-[Route("api/pujas")]
+[Route("api/subastas/{subastaId:int}/pujas")]
 public class PujasControlador : ControllerBase
 {
     // listado de pujas por subasta
-    [HttpGet("/api/subastas/{subastaId:int}/pujas")]
+    [HttpGet]
     public async Task<IActionResult> ListadoPorSubasta(int subastaId, [FromServices] ListadoPujasPorSubastaManejador manejador)
     {
         var resultado = await manejador.EjecucionAsync(new ListadoPujasPorSubastaConsulta(subastaId));
@@ -19,8 +19,9 @@ public class PujasControlador : ControllerBase
 
     // creacion de puja con reglas de negocio y transaccion
     [HttpPost]
-    public async Task<IActionResult> Creacion([FromBody] PujaRegistroComando comando, [FromServices] PujaRegistroManejador manejador)
+    public async Task<IActionResult> Creacion(int subastaId, [FromBody] PujaRegistroComando comando, [FromServices] PujaRegistroManejador manejador)
     {
+        comando.SubastaId = subastaId;
         var resultado = await manejador.EjecucionAsync(comando);
         return CreatedAtAction(nameof(ListadoPorSubasta), new { subastaId = resultado.SubastaId }, resultado);
     }
